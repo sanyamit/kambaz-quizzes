e82c1f6ee6d0d7f1bc48be3dae928aef0c4758d0
giaptl <156375308+giaptl@users.noreply.github.com>
2026-03-04 22:51:22 -0500

    fixed module buttons

--- app/(kambaz)/courses/[cid]/Modules/ModuleControlButtons.tsx
+++ app/(kambaz)/courses/[cid]/Modules/ModuleControlButtons.tsx
@@ -5,17 +5,20 @@ import { BsPlus } from "react-icons/bs";
 import { FaTrash } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 export default function ModuleControlButtons({
-  moduleId,
-  deleteModule,
-  editModule,
+  moduleId = "",
+  deleteModule = () => {},
+  editModule = () => {},
 }: {
-  moduleId: string;
-  deleteModule: (moduleId: string) => void;
-  editModule: (moduleId: string) => void }
-) {
+  moduleId?: string;
+  deleteModule?: (moduleId: string) => void;
+  editModule?: (moduleId: string) => void;
+}) {
   return (
     <div className="float-end">
-      <FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />
+      <FaPencil
+        onClick={() => editModule(moduleId)}
+        className="text-primary me-3"
+      />
       <FaTrash
         className="text-danger me-2 mb-1"
         onClick={() => deleteModule(moduleId)}
